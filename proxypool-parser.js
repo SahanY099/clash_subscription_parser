945225112a0097f64674a9e99094fb115ace27f2
Sahan Yashmika <88578622+SahanY099@users.noreply.github.com>
2023-11-18 03:07:57 +0530

    Create parser for proxypool

---
+++ proxypool-parser.js
@@ -0,0 +1,46 @@
+module.exports.parse = async (
+  raw,
+  { axios, yaml, notify, console },
+  { name, url, interval, selected }
+) => {
+  let config = yaml.parse(raw);
+  const host = "zoom.us";
+
+  for (let i = config.proxies.length - 1; i >= 0; i--) {
+    const proxy = config.proxies[i];
+
+    if (proxy.name.includes("198.244.252.93")) {
+      proxy.name = "H " + proxy.name;
+    }
+
+    // remove proxies with invalid ports
+    if (isNaN(proxy.port)) {
+      config.proxies.splice(i, 1);
+    }
+
+    if (proxy.type == "ss") config.proxies.splice(i, 1);
+    if (proxy.type == "ssr") config.proxies.splice(i, 1);
+
+    // add sni's to proxies
+    if (proxy.type == "trojan") {
+      proxy.sni = host;
+    }
+
+    if (proxy.type == "vmess") {
+      proxy.servername = host;
+
+      if (!proxy.tls) config.proxies.splice(i, 1);
+    }
+
+    if (proxy["ws-opts"]) {
+      proxy["ws-opts"]["headers"] = {
+        Host: "zoom.us",
+      };
+    }
+  }
+
+  config["proxy-groups"] = [];
+  config.rules = [];
+
+  return yaml.stringify(config);
+};
